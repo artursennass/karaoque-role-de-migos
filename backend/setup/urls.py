f8bc603e609b8aa25da8f0dece17512400028321
Abner Freitas <defreitasabner@gmail.com>
2023-03-24 13:06:04 -0300

    Implementada documentação automática da API utilizando Swagger.

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Karaokê",
+      default_version='v1',
+      description="Uma API de Karaokê",
+      terms_of_service="#",
+      contact=openapi.Contact(email="#"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 from apps.music_module.views import AuthorViewSet
 
@@ -8,8 +24,11 @@ router = routers.DefaultRouter()
 
 router.register('authors', AuthorViewSet, basename = 'Authors')
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path(
+            'doc/', schema_view.with_ui('swagger', cache_timeout=0), 
+            name='karaoke-api-documentation'
+        )
 ]
